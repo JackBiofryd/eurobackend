c854e4fcef1db05c51376e17994da500a0ca9bdf
unknown <jackbiofryd@gmail.com>
2021-06-14 20:54:01 +0200

    Fixed CORS

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		const { workType } = req.params;
