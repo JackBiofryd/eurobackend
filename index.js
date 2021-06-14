4addc40f4e3a94648acc40f429c2c07812b9dabd
unknown <jackbiofryd@gmail.com>
2021-06-14 21:40:00 +0200

    Added SSL Verification hopefully

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
